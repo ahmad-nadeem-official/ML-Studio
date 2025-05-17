9ad9cf540ec4c49097c70fa7f6b9c6cfe32cad33
ahmad-nadeem-official <ahmadnadeem701065@gmail.com>
2025-05-17 16:12:11 +0500

    initia

---
+++ main/main.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+print(dir(pd))
+print(pd.__version__)
+print(pd.__file__)
+print(pd.__path__)
+print(pd.__name__)
+print(pd.__package__)
+print(pd.__doc__)
+print(pd.__all__)
+print(pd.__builtins__)
+print(pd.__spec__)
+print(pd.__cached__)
+print(pd.__loader__)
